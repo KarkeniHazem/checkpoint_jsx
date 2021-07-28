18ae5992c3250a98adb59c5e3259f517b7982acb
KarkeniHazem <hazem.kerkerni1993@gmail.com>
2021-07-28 22:35:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styles from './style.css';
+import king from './king.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:"solid 1px black",maxWidth:"100vw"}}/>
+    <h1 className="title red">Hazem</h1>
+    <br />
+    <img src={king} alt="Logo" />;
+    {/* <img  width="100px"  height="100px" src="/king.jpg" ></img> */}
+    <br />
+    
+    <img src={process.env.PUBLIC_URL + '/hazem.png'} /> 
+    <br />
+    <iframe width="956" height="538" src="https://www.youtube.com/embed/i51SpN7jsck" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   );
 }
